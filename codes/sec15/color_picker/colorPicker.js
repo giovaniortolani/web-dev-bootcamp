25e8c39b0d4e8075d2eb8e02b9891a52ee93eb37
giovaniortolani <giovaniortolanibarbosa@gmail.com>
2018-06-21 14:46:41 -0300

    Color Picker Game done. Needs refactoring.

--- codes/sec15/color_picker/colorPicker.js
+++ codes/sec15/color_picker/colorPicker.js
@@ -7,31 +7,41 @@ let feedbackDisplay = document.getElementById("feedback-display");
 let newGameBtn = document.getElementById("btn-newgame");
 let easyBtn = document.getElementsByClassName("btn-difficulty")[1];
 let hardBtn = document.getElementsByClassName("btn-difficulty")[0];
+let difficulty = "hard"; // começa no hard
+
+let squaresLength = squares.length;
 
 let colors = [];
-let pickedColor;
+let pickedColor = null;
+
+let won = false;
 
 game();
 
 // Event Listener do clique e tratamento de acerto/erro do jogador
-for(let i = 0; i < squares.length; i++) {
+for(let i = 0; i < squaresLength; i++) {
     squares[i].addEventListener("click", function() {
-        // compara a cor do quadrado clicado com a cor correta
-        // let squareColor = this.style.backgroundColor;
-        if(colors[i] !== colors[pickedColor]){
-            feedbackDisplay.textContent = "Try Again!";
-            this.style.backgroundColor = document.body.style.backgroundColor;
-        }
-        else {
-            feedbackDisplay.textContent = "Correct!";
-            // muda a cor do header
-            titleDisplay.style.backgroundColor = this.style.backgroundColor;
-            // muda a cor dos quadrados
-            for(let j = 0; j < squares.length; j++) {
-                squares[j].style.backgroundColor = this.style.backgroundColor;
+        if(!won) {
+            // compara a cor do quadrado clicado com a cor correta
+            // let squareColor = this.style.backgroundColor;
+            if(colors[i] !== colors[pickedColor]){
+                feedbackDisplay.textContent = "Try Again!";
+                this.style.backgroundColor = document.body.style.backgroundColor;
             }
-            // muda o texto em newGameBtn 
-            newGameBtn.textContent = "Play Again?";
+            else {
+                feedbackDisplay.textContent = "Correct!";
+                titleDisplay.classList.add("color-transition");
+                // muda a cor do header
+                titleDisplay.style.backgroundColor = this.style.backgroundColor;
+                // muda a cor dos quadrados
+                for(let j = 0; j < squaresLength; j++) {
+                    squares[j].classList.add("color-transition");
+                    squares[j].style.backgroundColor = this.style.backgroundColor;
+                }
+                // muda o texto em newGameBtn 
+                newGameBtn.textContent = "Play Again?";
+                won = true;
+            }
         }
     }); 
 }
@@ -44,38 +54,45 @@ newGameBtn.addEventListener("click", function() {
 
 //
 easyBtn.addEventListener("click", function() {
-    // easyBtn.style.backgroundColor = "#3A73AA";
-    // easyBtn.style.color = "white";
-    // hardBtn.style.backgroundColor = "transparent";
-    // hardBtn.style.color = "#3A73AA";
     easyBtn.classList.add("btn-click");
     hardBtn.classList.remove("btn-click");
+    difficulty = "easy";
     reset();
     game();
 });
 
 //
 hardBtn.addEventListener("click", function() {
-    // hardBtn.style.backgroundColor = "#3A73AA";
-    // hardBtn.style.color = "white";
-    // easyBtn.style.backgroundColor = "transparent";
-    // easyBtn.style.color = "#3A73AA";
     hardBtn.classList.add("btn-click");
     easyBtn.classList.remove("btn-click");
+    difficulty = "hard";
     reset();
     game();
 });
 
 function game() {
+    if(difficulty === "hard") {
+        squaresLength = squares.length;
+        for(let i = 3; i < squares.length; i++) {
+            squares[i].style.visibility = "visible";
+        }
+    }
+    else if(difficulty === "easy") {
+        squaresLength = 3;
+        for(let i = 3; i < squares.length; i++) {
+            squares[i].style.visibility = "hidden";
+        }
+    }
+
     // Escolhe as cores dos quadrados e atribui-as
-    colors = generateRandomColors(squares.length);
-    for(let i = 0; i < squares.length; i++) {
+    colors = generateRandomColors(squaresLength);
+    for(let i = 0; i < squaresLength; i++) {
         // usar backgroundColor e não apenas background (mais compatível entre browsers)
         squares[i].style.backgroundColor = "rgb(" + colors[i][0] + ", " + colors[i][1] + ", " + colors[i][2] + ")";
     }
 
     // Escolhe uma cor "correta" e altera o título
-    pickedColor = pickColor(squares.length);
+    pickedColor = pickColor(squaresLength);
     for(let i = 0; i < rgbDisplay.length; i++) {
         rgbDisplay[i].textContent = colors[pickedColor][i]
     }
@@ -85,8 +102,13 @@ function reset() {
     newGameBtn.textContent = "New Colors";
     feedbackDisplay.textContent = "";
     titleDisplay.style.backgroundColor = "#3A73AA";
+    titleDisplay.classList.remove("color-transition");
+    for(let i = 0; i < squaresLength; i++) { 
+        squares[i].classList.remove("color-transition");
+    }
     colors = [];
     pickedColor = null;
+    won = false;
 }
 
 function generateRandomColors(numOfColors) {
